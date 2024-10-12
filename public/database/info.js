0ed3428490161b559d3e62299a6a186694e4a510
Minee <oakmuansuk123@gmail.com>
2024-10-12 15:14:32 +0700

    Add new file

---
+++ public/database/info.js
@@ -0,0 +1,137 @@
+const time = [
+  "08:00",
+  "09:00",
+  "10:00",
+  "11:00",
+  "12:00",
+  "13:00",
+  "14:00",
+  "15:00",
+  "16:00",
+  "17:00",
+  "18:00",
+  "19:00"
+];
+
+const subjectList = [
+  {
+    day: "Mon",
+    subject: [
+      {
+        subject: "Preparatory English",
+        startTime: 3,
+        hours: 2,
+        code: "0041001",
+        section: 57,
+        location: "RN1-805",
+        teacher: "Mr. Brian Akabagy Enyiawah",
+        bg: "#f472b6cc",
+        bgHover: "#BE185D",
+        detail: [
+          {
+            
+          }
+        ]
+      },
+      {
+        subject: "Math for fComputer System Design and ...",
+        startTime: 6,
+        hours: 3,
+        code: "1204102",
+        section: 1,
+        location: "IT-412",
+        teacher: "รศ.ดร.จันทิมา พลพินิจ",
+      },
+    ]
+  },
+  {
+    day: "Tue",
+    subject: [
+      {
+        subject: "Math for fComputer System Design and ...",
+        startTime: 2,
+        hours: 3,
+        code: "1204101",
+        section: 3,
+        location: "IT-413",
+        teacher: "รศ.ดร.นัฐธริยา เหล่าประชา"
+      },
+      {
+        subject: "Digital Society Legal and Ethics...",
+        startTime: 6,
+        hours: 2,
+        code: "1204104",
+        section: 2,
+        location: "IT-412",
+        teacher: "ผศ.ดร.สมนึก พ่วงพรพิทักษ์"
+      },
+    ]
+  },
+  {
+    day: "Wed",
+    subject: [
+      {
+        subject: "Problem Solving for Computer",
+        startTime: 1,
+        hours: 4,
+        code: "1204103",
+        section: 4,
+        location: "IT-402",
+        teacher: "ผศ.ดร.สำรวน เวียงสมุทร"
+      },
+      {
+        subject: "Digital Literacy and Life for Transformation",
+        startTime: 6,
+        hours: 2,
+        code: "0041022",
+        section: 23,
+        location: "SC1-300",
+        teacher: "อ.กวีพจน์ บรรลือวงศ์",
+        bg: "#f472b6cc",
+        bgHover: "#BE185D",
+      },
+      {
+        subject: "Cultural Management for Value Creation",
+        startTime: 11,
+        hours: 2,
+        code: "0045008",
+        section: 6,
+        location: "EN301",
+        teacher: "ผศ.ดร.เกรียงศักดิ์ บุญเที่ยง, อ.ทม เกตุวงศา, อ.ดร.ยุทธชัย เกราะแก้ว",
+        bg: "#C3B1E1",
+        bgHover: "#A068FF",
+      },
+    ]
+  },
+  {
+    day: "Thu",
+    subject: [
+      {
+        subject: "Creativity and Innovation Man...",
+        startTime: 6,
+        hours: 2,
+        code: "0043002",
+        section: 3,
+        location: "RN1-310",
+        teacher: "ผศ.ดร.นวลละออง อรรถรังสรรค์",
+        bg: "#C2F784",
+        bgHover: "#6EC207",
+      },
+      {
+        subject: "Logical Thinking Based Problem...",
+        startTime: 11,
+        hours: 2,
+        code: "0041025",
+        section: 2,
+        location: "RN1-409",
+        teacher: "อ.ดร.วิชญา รัตนเมธาวี, อ.เลอศักดิ์ โพธิ์ทอง",
+        bg: "#f472b6cc",
+        bgHover: "#BE185D",
+      },
+    ]
+  },
+  {
+    day: "Fri",
+    subject: []
+  },
+];
---
+++ public/js/script.js
@@ -0,0 +1,73 @@
+// auto scale
+
+// window.addEventListener("resize", () => {
+//   let size = window.innerWidth;
+//   document.getElementById("table").style.scale = size + "%";
+// });
+ 
+
+// dropdown
+const dropdownMenu = [
+  { id: 1, title: "โอ๊ค, ฟาง, วีโก้" },
+  { id: 2, title: "เอ" },
+  { id: 3, title: "ไนซ์" }
+];
+
+// Set default text on button
+document.getElementById("text-dropdown-btn").innerText = dropdownMenu[0].title; // Default to first item
+
+// menu in dropdown 
+dropdownMenu.forEach(item => {
+  // create menu from dropdownMenu array
+  const el = document.createElement('a');
+  el.href = "#";
+  el.className = "block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200 duration-200";
+  el.textContent = item.title;
+
+  document.getElementById("dropdown-menu").appendChild(el);
+  el.addEventListener("click", (e) => {
+    document.getElementById("text-dropdown-btn").innerText = e.target.textContent;
+    document.getElementById("dropdown-menu").classList.add("hidden");
+  });
+});
+
+// open menu dropdown
+document.getElementById("dropdown-btn").addEventListener("click", (e) => {
+  e.stopPropagation(); // Prevent click event from propagating to the document
+  const dropdownMenu = document.getElementById("dropdown-menu");
+
+  dropdownMenu.classList.toggle("hidden");
+
+  if (!dropdownMenu.classList.contains("hidden")) {
+    document.addEventListener("click", closeDropdownOnClickOutside);
+    // click outside dropdown for close
+    function closeDropdownOnClickOutside(e) {
+      const dropdownMenu = document.getElementById("dropdown-menu");
+      const dropdownBtn = document.getElementById("dropdown-btn");
+
+      if (!dropdownMenu.contains(e.target) && !dropdownBtn.contains(e.target)) {
+        dropdownMenu.classList.add("hidden");
+        document.removeEventListener("click", closeDropdownOnClickOutside); // Remove event listener after dropdown is closed
+      }
+    }
+  }
+});
+
+// ********** modal **********
+// open
+document.getElementById("open-model-btn").addEventListener("click", () => {
+  document.getElementById("modal").classList.remove("invisible");
+  document.getElementById("modal").classList.remove("opacity-0");
+  document.getElementById("modal").classList.add("opacity-100");
+  document.getElementById("modal-content").classList.remove("scale-[0.9]");
+});
+
+// close
+document.querySelectorAll(".close-modal").forEach(el => {
+  el.addEventListener("click", () => {
+    document.getElementById("modal").classList.add("opacity-0");
+    document.getElementById("modal").classList.remove("opacity-100");
+    document.getElementById("modal").classList.add("invisible");
+    document.getElementById("modal-content").classList.add("scale-[0.9]");
+  });
+});
