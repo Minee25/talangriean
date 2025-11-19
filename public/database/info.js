cff8ed0099f1feada39f50ea119efab79d4808e0
Minee <oakmuansuk123@gmail.com>
2025-11-19 14:56:57 +0700

    Add time table
    Add Mon and Fang

--- public/database/info.js
+++ public/database/info.js
@@ -51,7 +51,7 @@ const subjectList = [
             nameThai: "โครงสร้างข้อมูล",
             time: "10:00 - 12:00",
             code: "1204207",
-            section: 1,
+            section: 5,
             room: "IT-412",
             teacher: "ผศ.ดร.ฉัตรเกล้า เจริญผล",
             classRoomLink: "https://classroom.google.com/c/ODE3NTIyMjg1MTY2",
@@ -100,7 +100,277 @@ const subjectList = [
             nameThai: "โครงสร้างข้อมูล",
             time: "17:00 - 19:00",
             code: "1204207",
+            section: 5,
+            room: "IT-412",
+            teacher: "ผศ.ดร.ฉัตรเกล้า เจริญผล",
+            classRoomLink: "https://classroom.google.com/c/ODE3NTIyMjg1MTY2",
+            links: [
+              "https://www.facebook.com/share/g/1LBvevQxCD/"
+            ],
+            img: "",
+            bg: "#D5E6FF",
+            bgHover: "#000080",
+          },
+        ]
+      },
+      {
+        day: "Fri",
+        classes: [
+          {
+            name: "Computer Network Administration",
+            nameThai: "โครงสร้างข้อมูล",
+            time: "08:00 - 12:00",
+            code: "1204413",
             section: 1,
+            room: "IT-401",
+            teacher: "อ.ดร.อรรถพล สุวรรณษา",
+            classRoomLink: "https://classroom.google.com/c/ODI4MjYwNTUyNTYx",
+            links: [],
+            img: "",
+            bg: "#D5E6FF",
+            bgHover: "#000080",
+          },
+          {
+            name: "Human Computer Interaction",
+            nameThai: "โครงสร้างข้อมูล",
+            time: "13:00 - 16:00",
+            code: "1204209",
+            section: 2,
+            room: "IT-412",
+            teacher: "อ.ดร.เบญจวรรณ อินทระ",
+            classRoomLink: "https://classroom.google.com/c/ODE3NTIzNTcwMjMw",
+            links: [],
+            img: "",
+            bg: "#D5E6FF",
+            bgHover: "#000080",
+          },
+        ]
+      },
+    ]
+  },
+  {
+    id: "1",
+    title: "Mon",
+    schedule: [
+      {
+        day: "Mon",
+        classes: [
+          {
+            name: "Web Development and Management",
+            nameThai: "การพัฒนาและการจัดการเว็บ",
+            time: "13:00 - 17:00",
+            code: "1204204",
+            section: 2,
+            room: "IT-403",
+            teacher: "อ.พชระ พฤกษะศรี",
+            classRoomLink: "https://classroom.google.com/c/ODE3NTIzNjYxOTA5",
+            links: [
+              "https://www.facebook.com/groups/2401278043640275"
+            ],
+            img: "",
+            bg: "#D5E6FF",
+            bgHover: "#000080",
+          }
+        ]
+      },
+      {
+        day: "Tue",
+        classes: [
+          {
+            name: "Numerical Method for Computer Science",
+            nameThai: "วิธีการเชิงตัวเลขสำหรับวิทยาการคอมพิวเตอร์",
+            time: "13:00 - 17:00",
+            code: "1204201",
+            section: 4,
+            room: "IT-402",
+            teacher: "รศ.ดร.สุชาติ คุ้มมะณี",
+            classRoomLink: "https://classroom.google.com/c/ODE3NTIzODYyNjQ1",
+            links: [],
+            img: "",
+            bg: "#D5E6FF",
+            bgHover: "#000080",
+          }
+        ]
+      },
+      {
+        day: "Wed",
+        classes: [
+          {
+            name: "Data Structures",
+            nameThai: "โครงสร้างข้อมูล",
+            time: "10:00 - 12:00",
+            code: "1204207",
+            section: 5,
+            room: "IT-412",
+            teacher: "ผศ.ดร.ฉัตรเกล้า เจริญผล",
+            classRoomLink: "https://classroom.google.com/c/ODE3NTIyMjg1MTY2",
+            links: [
+              "https://www.facebook.com/share/g/1LBvevQxCD/"
+            ],
+            img: "",
+            bg: "#D5E6FF",
+            bgHover: "#000080",
+          },
+          {
+            name: "Citizenship for Well-Being",
+            nameThai: "การเป็นพลเมืองเพื่อความเป็นอยู่ที่ดี",
+            time: "13:00 - 15:00",
+            code: "0044003",
+            section: 2,
+            room: "RN1-604",
+            teacher: "รศ.วชิรวัตติ์ อาริยะสิริโชติ",
+            classRoomLink: "",
+            links: [],
+            img: "",
+            bg: "#FED7AA",
+            bgHover: "#C2410C",
+          },
+          {
+            name: "Cannabis Science",
+            nameThai: "กัญชาวิทยา",
+            time: "15:00 - 17:00",
+            code: "0042013",
+            section: 1,
+            room: "SC2-102",
+            teacher: "รศ.ดร.เกรียงศักดิ์ บุญเที่ยง, ผศ.ดร.ประภัสสร บุษหมั่น",
+            classRoomLink: "https://classroom.google.com/c/ODIwMzUxNDM5ODcz",
+            links: [],
+            img: "",
+            bg: "#FED7AA",
+            bgHover: "#C2410C",
+          },
+        ]
+      },
+      {
+        day: "Thu",
+        classes: [
+          {
+            name: "Data Structures",
+            nameThai: "โครงสร้างข้อมูล",
+            time: "17:00 - 19:00",
+            code: "1204207",
+            section: 5,
+            room: "IT-412",
+            teacher: "ผศ.ดร.ฉัตรเกล้า เจริญผล",
+            classRoomLink: "https://classroom.google.com/c/ODE3NTIyMjg1MTY2",
+            links: [
+              "https://www.facebook.com/share/g/1LBvevQxCD/"
+            ],
+            img: "",
+            bg: "#D5E6FF",
+            bgHover: "#000080",
+          },
+        ]
+      },
+      {
+        day: "Fri",
+        classes: [
+          {
+            name: "Computer Network Administration",
+            nameThai: "โครงสร้างข้อมูล",
+            time: "08:00 - 12:00",
+            code: "1204413",
+            section: 1,
+            room: "IT-401",
+            teacher: "อ.ดร.อรรถพล สุวรรณษา",
+            classRoomLink: "https://classroom.google.com/c/ODI4MjYwNTUyNTYx",
+            links: [],
+            img: "",
+            bg: "#D5E6FF",
+            bgHover: "#000080",
+          },
+          {
+            name: "Human Computer Interaction",
+            nameThai: "โครงสร้างข้อมูล",
+            time: "13:00 - 16:00",
+            code: "1204209",
+            section: 2,
+            room: "IT-412",
+            teacher: "อ.ดร.เบญจวรรณ อินทระ",
+            classRoomLink: "https://classroom.google.com/c/ODE3NTIzNTcwMjMw",
+            links: [],
+            img: "",
+            bg: "#D5E6FF",
+            bgHover: "#000080",
+          },
+        ]
+      },
+    ]
+  },
+  {
+    id: "2",
+    title: "Fang",
+    schedule: [
+      {
+        day: "Mon",
+        classes: [
+          {
+            name: "Web Development and Management",
+            nameThai: "การพัฒนาและการจัดการเว็บ",
+            time: "13:00 - 17:00",
+            code: "1204204",
+            section: 2,
+            room: "IT-403",
+            teacher: "อ.พชระ พฤกษะศรี",
+            classRoomLink: "https://classroom.google.com/c/ODE3NTIzNjYxOTA5",
+            links: [
+              "https://www.facebook.com/groups/2401278043640275"
+            ],
+            img: "",
+            bg: "#D5E6FF",
+            bgHover: "#000080",
+          }
+        ]
+      },
+      {
+        day: "Tue",
+        classes: [
+          {
+            name: "Numerical Method for Computer Science",
+            nameThai: "วิธีการเชิงตัวเลขสำหรับวิทยาการคอมพิวเตอร์",
+            time: "13:00 - 17:00",
+            code: "1204201",
+            section: 4,
+            room: "IT-402",
+            teacher: "รศ.ดร.สุชาติ คุ้มมะณี",
+            classRoomLink: "https://classroom.google.com/c/ODE3NTIzODYyNjQ1",
+            links: [],
+            img: "",
+            bg: "#D5E6FF",
+            bgHover: "#000080",
+          }
+        ]
+      },
+      {
+        day: "Wed",
+        classes: [
+          {
+            name: "Data Structures",
+            nameThai: "โครงสร้างข้อมูล",
+            time: "10:00 - 12:00",
+            code: "1204207",
+            section: 5,
+            room: "IT-412",
+            teacher: "ผศ.ดร.ฉัตรเกล้า เจริญผล",
+            classRoomLink: "https://classroom.google.com/c/ODE3NTIyMjg1MTY2",
+            links: [
+              "https://www.facebook.com/share/g/1LBvevQxCD/"
+            ],
+            img: "",
+            bg: "#D5E6FF",
+            bgHover: "#000080",
+          },
+        ]
+      },
+      {
+        day: "Thu",
+        classes: [
+          {
+            name: "Data Structures",
+            nameThai: "โครงสร้างข้อมูล",
+            time: "17:00 - 19:00",
+            code: "1204207",
+            section: 5,
             room: "IT-412",
             teacher: "ผศ.ดร.ฉัตรเกล้า เจริญผล",
             classRoomLink: "https://classroom.google.com/c/ODE3NTIyMjg1MTY2",
